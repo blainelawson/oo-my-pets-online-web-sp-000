749b097670d0b926bca2e679d531cdbae7b52686
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 21:11:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,9 +50,9 @@ class Owner
   end
 
   def sell_pets
-    @pets[:dogs].each do |dog| {dog.mood = "nervous"}
-    @pets[:cats].each do |cat| {cat.mood = "nervous"}
-    @pets[:fishes].each do |fish| {fish.mood = "nervous"}
+    @pets[:dogs].each {|dog| dog.mood = "nervous"}
+    @pets[:cats].each {|cat| cat.mood = "nervous"}
+    @pets[:fishes].each {|fish| fish.mood = "nervous"}
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
