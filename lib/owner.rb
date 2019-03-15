839b72c91e58708e8919b7ab63b34ae7e665b926
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 20:35:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -21,6 +21,16 @@ class Owner
     @pets[:fishes] << fish
   end
 
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
   def say_species
     "I am a #{@species}."
   end
